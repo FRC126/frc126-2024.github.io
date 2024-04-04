a4449df919603ddda1771428937bf29ebb74203f
Tom Weissinger <tweissin@gmail.com>
2024-04-04 10:26:36 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -16,7 +16,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024rikin",
+      "defaultValue": "2024necmp",
       "required": "true"
     },
     { "name": "Match Level",
